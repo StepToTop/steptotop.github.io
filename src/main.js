553fa460e0972a3bb757df811fbd1828dd67ae36
StepToTop <1185139452@qq.com>
2020-01-01 12:05:21 +0800

    学习

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+// import Vue from 'vue/dist/vue.esm'
 import App from './App.vue'
 
 
 new Vue({
-    el: '#app',
-    render: h => h(App)
+    el: '#app',  // 用于挂载要管理的元素
+    render: h => h(App),  // 
+    // data: { // 定义数据
+    //     message: "牛大了"
+    // }
 });
